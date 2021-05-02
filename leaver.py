63a4f4919828b2e2af508e51ba0378572bbcd38a
Fischi <61426239+Beezyboy@users.noreply.github.com>
2021-05-02 13:44:17 +0200

    Add files via upload

---
+++ leaver.py
@@ -0,0 +1,34 @@
+import os
+import requests
+from tkinter import *
+from tkinter.filedialog import *
+
+#opens tokens
+
+print ("Open Token .txt file...")
+Tk().withdraw()
+token = askopenfilename()
+with open(token) as handle:
+    txtf = handle.read()
+
+f = open("tokens.txt","w+")
+f.write(txtf)
+f.close()
+
+#leaves Discord Server
+
+ID = input('Discord Server ID to leave: ')
+
+apilink = "https://discordapp.com/api/v6/users/@me/guilds/" + str(ID)
+
+with open('tokens.txt','r') as handle:
+        tokens = handle.readlines()
+        for x in tokens:
+            token = x.rstrip()
+            headers={
+                'Authorization': token
+                }
+            requests.delete(apilink, headers=headers)
+        print ("All valid tokens have left that server.")
+os.remove("tokens.txt")
+input()
